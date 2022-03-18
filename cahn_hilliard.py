5fc3f957054947389936203395a5751ce0b54868
Jonathan Guyer <guyer@nist.gov>
2022-03-17 23:07:05 -0400

    Add arguments and timing

--- cahn_hilliard.py
+++ cahn_hilliard.py
@@ -1,19 +1,28 @@
 import argparse
 import os
+import time
 
+start = time.time()
+
+times = []
 
 parser = argparse.ArgumentParser(description='Solve a Cahn-Hillard problem')
 parser.add_argument('--nx', dest='nx', type=int, default=100,
                     help='number of cells in x direction (default: 100)')
 parser.add_argument('--ny', dest='ny', type=int, default=100,
                     help='number of cells in y direction (default: 100)')
+parser.add_argument('--steps', dest='steps', type=int, default=10,
+                    help='number of time steps (default: 10)')
+parser.add_argument('--sweeps', dest='sweeps', type=int, default=5,
+                    help='number of sweeps (default: 5)')
+parser.add_argument('--solver', dest='solver', type=str, default="petsc",
+                    help='solver suite (default: "petsc")')
 
 args = parser.parse_args()
 
+os.environ["FIPY_SOLVERS"] = args.solver
 
 import fipy as fp
-
-os.environ["FIPY_SOLVERS"] = "petsc"
 
 mesh = fp.GmshGrid2D(nx=args.nx, ny=args.ny, dx=0.25, dy=0.25)
 phi = fp.CellVariable(name=r"$\phi$", mesh=mesh)
@@ -39,7 +48,12 @@ eq3 = (fp.ImplicitSourceTerm(coeff=1., var=psi)
        - fp.DiffusionTerm(coeff=epsilon**2, var=phi))
 eq = eq1 & eq2
 
+times.append(time.time() - start)
+
 dt = 1e2
-for step in range(10):
-    for sweep in range(5):
+for step in range(args.steps):
+    for sweep in range(args.sweeps):
         res = eq.sweep(dt=dt)
+    times.append(time.time() - start)
+
+print("\t".join([str(t) for t in times]))
