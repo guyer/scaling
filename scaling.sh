23b43dd4f17adb56fecd4f6787e91b9cf8800376
Jonathan Guyer <guyer@nist.gov>
2022-03-19 12:24:40 -0400

    Accept arguments
    Including what type of scaling, what simulation, and simulation size

--- scaling.sh
+++ scaling.sh
@@ -3,10 +3,18 @@
 #SBATCH --array=0-6%1
 #SBATCH --time=12:00
 
-queue=fast
+scaling=$1
+script=$2
+nx=$3
+ny=$4
 
 ntasks=$((2 ** SLURM_ARRAY_TASK_ID))
+if  [ $scaling == "weak" ]; then
+    ny=$((ny * ntasks))
+fi
 
-sbatch --partition=$SBATCH_PARTITION --job-name=$ntasks-strong --ntasks=$ntasks --ntasks-per-core=2 \
-  --output=${PWD}/results/$SBATCH_PARTITION/strong-$nsize.slurmout \
-  jobscript
+jobname=$scaling-$nx-$ny-$ntasks
+
+sbatch --partition=$SBATCH_PARTITION --job-name=$jobname --ntasks=$ntasks --ntasks-per-core=2 \
+  --output=${PWD}/results/$SBATCH_PARTITION/$jobname.slurmout \
+  jobscript $script $nx $ny
