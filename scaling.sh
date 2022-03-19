c7dcd822ea8cb4d6d7fa8a0dd097d063a24196b9
Jonathan Guyer <guyer@nist.gov>
2022-03-19 12:23:25 -0400

    Use slurm array to sbatch individual jobs

--- scaling.sh
+++ scaling.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+#SBATCH --array=0-6%1
 #SBATCH --time=12:00
 
 queue=fast
 
-for n in {0..6}; do
-    size=$((2 ** n));
-    id=`sbatch --partition=$queue --job-name=$size-strong --ntasks=$size --ntasks-per-core=2 --output=${PWD}/results/$queue/strong-$size.slurmout jobscript`
-    echo "ntasks $n jobid $id size $size"
-done
+ntasks=$((2 ** SLURM_ARRAY_TASK_ID))
+
+sbatch --partition=$SBATCH_PARTITION --job-name=$ntasks-strong --ntasks=$ntasks --ntasks-per-core=2 \
+  --output=${PWD}/results/$SBATCH_PARTITION/strong-$nsize.slurmout \
+  jobscript
